37e29656f551b273649eb233c91253806bb2286e
PraneethJain <praneethjain005@gmail.com>
2022-07-02 17:50:55 +0530

    Formatted code

--- anilist/authenticate.py
+++ anilist/authenticate.py
@@ -1,9 +1,13 @@
 def save_token():
     import webbrowser
-    url = f"https://anilist.co/api/v2/oauth/authorize?client_id=8068&response_type=token"
+
+    url = (
+        f"https://anilist.co/api/v2/oauth/authorize?client_id=8068&response_type=token"
+    )
     webbrowser.open(url)
     with open(r".\anilist\access_token.txt", "w") as f:
         f.write(input("Paste the access token here: "))
+
 
 def get_token():
     with open(r".\anilist\access_token.txt") as f:
