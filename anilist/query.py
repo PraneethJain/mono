b1c0e5b62944f296876f60beb21c1addca1ae59f
PraneethJain <praneethjain005@gmail.com>
2022-07-02 18:11:03 +0530

    Finish get_user_list function

--- anilist/query.py
+++ anilist/query.py
@@ -35,7 +35,45 @@ def get_user_data():
     }
     """
     data = requests.post(url, json={"query": query}, headers=get_headers())
-    return json.loads(data.text)["data"]
+    return json.loads(data.text)["data"]["Viewer"]
 
 
-print(get_user_data())
+def get_user_list(status: str):
+    query = """
+    query ($userId: Int, $status: MediaListStatus) {
+        MediaListCollection (userId: $userId, status: $status, type: ANIME) {
+            lists {
+                name
+                entries {
+                    media {
+                        id
+                        title {
+                            romaji
+                            english
+                            native
+                            userPreferred
+                        }
+                        format
+                        status
+                        description (asHtml: false)
+                        startDate {day month year}
+                        endDate {day month year}
+                        season
+                        seasonYear
+                        episodes
+                        duration
+                        source
+                    }
+                }
+            }
+        }
+    }
+    """
+    variables = {"userId": get_user_data()["id"], "status": status}
+    data = requests.post(
+        url, json={"query": query, "variables": variables}, headers=get_headers()
+    )
+    return json.loads(data.text)["data"]["MediaListCollection"]["lists"][0]
+
+
+print(get_user_list("CURRENT"))
