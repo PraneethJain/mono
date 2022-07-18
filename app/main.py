0f2e410d707143175a7037c324015665d902a3d3
PraneethJain <praneethjain005@gmail.com>
2022-07-18 22:37:04 +0530

    Implemented basic app layout

---
+++ app/main.py
@@ -0,0 +1,43 @@
+from rich.markdown import Markdown
+from rich.panel import Panel
+from rich.style import Style
+from textual.app import App
+from textual.reactive import Reactive
+from textual.widget import Widget
+from textual.widgets import Header, Footer, ScrollView, Placeholder, Static
+
+from anilist.query import get_user_list
+
+class PanelList(Widget):
+    
+    def __init__(self, content: list, style: Style = "default on default", name: str | None = None) -> None:
+        super().__init__(name)
+        self.string = self.process(content)
+        self.style = style
+    
+    def process(self, content):
+        s = ''
+        for i,ele in enumerate(content, start=1):
+            s += f"{i}. {ele}"
+        return s
+
+    
+    def render(self) -> Panel:
+        return Panel(self.string, style = self.style)
+
+
+class Mono(App):
+    
+    async def on_load(self, event) -> None:
+        await self.bind('q', 'quit', 'Quit')
+        
+    async def on_mount(self, event) -> None:
+        self.header = Header(style="#FD7F20 on default")
+        self.footer = Footer()
+        self.shows = PanelList([entry['media']['title']['romaji']+'\n' for entry in get_user_list("CURRENT")['entries']], style="#FDB750 on default")
+        await self.view.dock(self.header, edge="top")
+        await self.view.dock(self.footer, edge="bottom")
+        await self.view.dock(self.shows, edge="left", size=40)
+        await self.view.dock(Placeholder(), Placeholder(),edge="top")
+
+Mono.run(title="Mono", log="textual.log")
