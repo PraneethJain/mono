32e94da1c47ec5c139a4433ac9cc818881a1c430
PraneethJain <praneethjain005@gmail.com>
2022-07-20 22:22:05 +0530

    Removed debug print statement

--- app/scraper.py
+++ app/scraper.py
@@ -5,7 +5,6 @@ from rich import print
 def find_magnet(title: str):
     title = title.replace('2nd Season', 'S2')
     search_term = ' '.join(title.split()[:-2])+' - '+f"{int(title.split()[-1]):02}"
-    print(search_term)
     url = f"https://www.tokyotosho.info/rss.php?terms={search_term.replace(' ','+')}&type=1&searchName=true&searchComment=true&size_min=&size_max=&username="
     soup = BeautifulSoup(requests.get(url).text)
     options = {}
