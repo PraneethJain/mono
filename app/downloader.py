8791f2e9121a5cffb6def67b1be7c2dca07afc23
PraneethJain <praneethjain005@gmail.com>
2022-07-22 14:11:37 +0530

    Moved client to class variable and added useful functions

--- app/downloader.py
+++ app/downloader.py
@@ -3,18 +3,35 @@ from rich import print
 from scraper import find_magnet
 
 class Torrent:
+    
+    client = Client("http://127.0.0.1:8080/")
+    client.login("admin", "adminadmin")
 
     def __init__(self, series, magnet) -> None:
         self.series = series
         self.magnet = magnet
-        self.client = Client("http://127.0.0.1:8080/")
-        self.client.login("admin", "adminadmin")
         self.download()
         
     def download(self) -> None:
         self.client.download_from_link(self.magnet, savepath=f"D:\\Torrents\\Anime\\{self.series}", category="Anime")
         self.infohash = self.client.torrents(category="Anime", sort="added_on", reverse=True)[0]["hash"]
-        self.data = self.client.get_torrent(self.infohash)
         
-    def get_progress(self) -> None:
-        return round(self.data['total_downloaded']/self.data['total_size']*100,2)
+    def get_infohash(self) -> None:
+        return self.infohash
+
+    @classmethod
+    def get_progress(cls, infohash) -> None:
+        data = cls.client.get_torrent(infohash)
+        return round(data['total_downloaded']/data['total_size']*100,2)
+    
+    @classmethod
+    def pause_torrent(cls, infohash) -> None:
+        cls.client.pause(infohash)
+        
+    @classmethod
+    def resume_torrent(cls, infohash) -> None:
+        cls.client.resume(infohash)
+    
+    @classmethod
+    def get_torrent(cls, infohash) -> None:
+        return cls.client.get_torrent(infohash) 
