f1340388991972532811a24dc09c88e56b8a22dd
PraneethJain <praneethjain005@gmail.com>
2023-03-13 16:39:39 +0530

    Switched source

--- scrape.py
+++ scrape.py
@@ -7,39 +7,31 @@ from edge_cases import mappings
 
 
 class Scraper:
-    url = "https://nyaa.si/?f=2&c=1_2&q="
-
     def __init__(self) -> None:
         self.client = httpx.AsyncClient()
 
-    async def find_magnets(self, series: str, episode_number: int) -> str:
+    async def find_magnet(self, series: str, episode_number: int) -> str:
         if series in mappings:
             episode_number += mappings[series][1]
             series = mappings[series][0]
         elif "Season " in series:
             series = series.replace("Season ", "S")
-        search_term = f"{series} - {episode_number:02d}"
-        url = self.url + search_term.replace(" ", "+")
+        search_term = f"{series} - {episode_number:02d} 1080p"
+        url = f'https://tokyotosho.org/search.php?terms={search_term.replace(" ", "+")}&type=1&searchName=true&username=subsplease'
+
         r = await self.client.get(url)
         soup = BeautifulSoup(r.text, "html.parser")
 
-        magnets = {}
-        found_first = False
-        for node in soup.select(".success"):
-            title = node.select("td")[1].select("a")[-1].attrs["title"]
-            magnet = node.select("td")[2].select("a")[-1].attrs["href"]
-            magnets[title] = magnet
-            if (
-                all(
-                    keyword in title
-                    for keyword in ["[SubsPlease]", "1080p", f" {episode_number:02d} "]
-                )
-                and not found_first
-            ):
-                magnets["first"] = (title, magnet)
-                found_first = True
+        for node in soup.select(".sprite_magnet"):
+            episode_data = node.parent.parent.select("a")
+            magnet = episode_data[0].attrs["href"]
+            title = episode_data[1].text
+            title = title[:-4] + title[-3:]
 
-        return magnets
+            if f" {episode_number:02d} " in title:
+                return (title, magnet)
+
+        return None
 
     async def close(self) -> None:
         await self.client.aclose()
@@ -51,7 +43,7 @@ scraper = Scraper()
 # async def main():
 #     scraper = Scraper()
 #     series = "Otonari no Tenshi-sama ni Itsunomanika Dame Ningen ni Sareteita Ken"
-#     print(await scraper.find_magnets(series, 8))
+#     print(await scraper.find_magnet(series, 8))
 #     await scraper.close()
 
 
