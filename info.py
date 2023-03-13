949bf88a73f722bb370390532a058bf032398ce9
PraneethJain <praneethjain005@gmail.com>
2023-03-13 16:29:31 +0530

    printing paths

--- info.py
+++ info.py
@@ -9,3 +9,6 @@ data_path = os.path.join(data_dir, "data.json")
 
 cache_dir = platformdirs.user_cache_dir(app_name, author_name)
 cache_path = os.path.join(cache_dir, "cache.json")
+
+if __name__ == "__main__":
+    print(f"{data_dir=} {cache_dir=}")
